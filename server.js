9114c2b4d496ff235e5ae9af4adfeac39a58e0b5
sarthak <sarthajk@123>
2025-08-22 14:49:24 +0530

    2nd commit

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:3000", // frontend URL
+     origin: process.env.CLIENT_URL || "http://localhost:3000", // frontend URL
     methods: ["GET", "POST"],
     credentials: true,
   },
